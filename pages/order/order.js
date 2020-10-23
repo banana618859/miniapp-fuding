39b0e4ac7df638b82ba38e55553b0f8d772e2afe
yiz_seeing <yizheng.yuan@gzseeing.com>
2020-10-23 23:34:19 +0800

    订单列表
    订单列表

--- pages/order/order.js
+++ pages/order/order.js
@@ -5,13 +5,109 @@ Page({
    * 页面的初始数据
    */
   data: {
+    winWidth: 0,
+    winHeight: 0,
+    currentTab: 0,
+    allOrder:[
+      {
+        id: 1,
+        shopImg: '../../images/category/ms.png',
+        shopName: '刘源螺蛳粉',
+        orderStatus: '已送达',
+        shopStatus: false,
+        totalPrice: 15.0
+      },
+      {
+        id: 2,
+        shopImg: '../../images/logo.jpg',
+        shopName: '隆江猪脚饭',
+        orderStatus: '已送达',
+        shopStatus: true,
+        totalPrice: 38.0
+      },
+      {
+        id: 3,
+        shopImg: '../../images/category/sg.png',
+        shopName: '三鲜快餐店',
+        orderStatus: '已送达',
+        shopStatus: true,
+        totalPrice: 26.0
+      }
+    ],
+    allWaitCommentOrder:[
+      {
+        id: 1,
+        shopImg: '../../images/category/sg.png',
+        shopName: '三鲜快餐店',
+        orderStatus: '已送达',
+        shopStatus: true,
+        totalPrice: 26.0
+      },
+      {
+        id: 2,
+        shopImg: '../../images/category/ms.png',
+        shopName: '刘源螺蛳粉',
+        orderStatus: '已送达',
+        shopStatus: false,
+        totalPrice: 15.0
+      }
+    ],
+    allQuitOrder:[
+      {
+        id: 1,
+        shopImg: '../../images/logo.jpg',
+        shopName: '隆江猪脚饭',
+        orderStatus: '已送达',
+        shopStatus: true,
+        totalPrice: 38.0
+      },
+      {
+        id: 2,
+        shopImg: '../../images/category/sg.png',
+        shopName: '三鲜快餐店',
+        orderStatus: '已送达',
+        shopStatus: true,
+        totalPrice: 26.0
+      }
+    ]
+  },
+  onLoad: function() {
 
+      var that = this;
+
+      /**
+       * 获取当前设备的宽高
+       */
+      wx.getSystemInfo( {
+
+          success: function( res ) {
+              that.setData( {
+                  winWidth: res.windowWidth,
+                  winHeight: res.windowHeight
+              });
+          }
+
+      });
+  },
+    
+  //  tab切换逻辑
+  swichNav: function( e ) {
+
+      var that = this;
+
+      if( this.data.currentTab === e.target.dataset.current ) {
+          return false;
+      } else {
+          that.setData( {
+              currentTab: e.target.dataset.current
+          })
+      }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  bindChange: function( e ) {
+
+      var that = this;
+      that.setData( { currentTab: e.detail.current });
 
   },
 
